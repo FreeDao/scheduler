f75bd19421cc73176e7f40554450f4e6902475b5
liufeiit <liufei_it@126.com>
2013-10-28 15:41:38 +0800

    s

--- src/main/java/com/ruoogle/nova/ITester.java
+++ src/main/java/com/ruoogle/nova/ITester.java
@@ -9,6 +9,11 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 /**
  * 
@@ -17,8 +22,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @since 2013年9月28日 下午5:33:45
  */
 public class ITester {
+	
+	public static void main(String[] args) {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://172.16.10.79:3306/ruooglenova?useUnicode=true&characterEncoding=utf-8");
+		dataSource.setUsername("nova");
+		dataSource.setPassword("123456");
+		
+		
+		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+		databasePopulator.addScript(new ClassPathResource("/rank.sql"));
+		DatabasePopulatorUtils.execute(databasePopulator, dataSource);
+		
+	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main0(String[] args) throws Exception {
 		ApplicationContext context = new ClassPathXmlApplicationContext("/schedule/job/*.xml");
 		JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
 		Job job = context.getBean("marketLevelJob", Job.class);
---
+++ src/main/resources/rank.sql
@@ -0,0 +1,34 @@
+delete from ruooglenova.nova_user_rank_list;
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("魅力榜", 2, 1, 1, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("富豪榜", 2, 1, 2, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("好评榜", 2, 1, 3, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("主播榜", 2, 1, 4, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("视频榜", 2, 1, 5, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("女神榜", 2, 1, 6, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("女汉子", 2, 1, 7, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("软妹子", 2, 1, 8, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("小清新", 2, 1, 9, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("新来乍到", 2, 1, 10, "", now() , now() , "");
+
+INSERT INTO ruooglenova.nova_user_rank_list(rank_name, rank_type, show_type, rank_index, icon, gmt_create, gmt_modify, extend) 
+VALUES ("随机推荐", 2, 1, 11, "", now() , now() , "");
